1432f9aaafae7769528d550974d4d87bffd6b04a
zer0plus <mitanshd108@gmail.com>
2021-02-15 22:12:04 -0500

    working ath

--- yearSpread_alerts.py
+++ yearSpread_alerts.py
@@ -8,20 +8,30 @@ def process(inp_wl):
     yrSpread_dict = {}
     #Makes a dictionary of Price : 52-week spread
     for tkr in w1:
+        tempp = []
         for k, v in (tkr.items()):
+            if (k == '$ticker'):
+                tempp.append(v)
             if (k == 'price'):
-                tempp = v 
+                tempp.append(v)    
             if (k == '52 Week-range'):
                 hooh = (v.split(' -'))
-                yrSpread_dict[tempp] = hooh
+        yrSpread_dict[tuple(tempp)] = hooh
+                
+    for k, v in yrSpread_dict.items():
+        if(float(k[1]) >= (0.9 * float(v[1]))):
+            print('Stock: ' + str(k[0]) + ' is trading near ATH')
+        print('min requiried: ' + str(0.9 * float(v[1])))
+        print(round((float(v[1]) - float(k[1])), 2))
     return w1, yrSpread_dict
 
 def main():
-    spacs = ['GSAH', 'GIK', 'APXT', 'THCB', 'TTCF']
+    spacs = ['GSAH', 'GIK', 'APXT', 'THCB', 'TTCF', 'CCIV']
     soup = process(spacs)
     main_soup = soup[0]
     s2 = soup[1]
-    print(main_soup)
+    # print(main_soup)
+    print('\n')
     print(s2)
     with open('dailyreport.json', 'w') as df:
         for tkr in main_soup:
